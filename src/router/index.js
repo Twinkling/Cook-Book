2297b6bc6cc82e046532220147d8a0e7e3297616
Twinkling <twinkling8023@gmail.com>
2019-04-09 11:11:42 +0800

    fix: 修改路由模式以在 github pages 上使用

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import demoRouter from './demo';
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         homeRouter,
